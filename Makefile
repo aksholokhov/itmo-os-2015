f02d51de9cf140d997367f4c39732fc73285e119
Alex Sholokhov <ak.sholokhov@gmail.com>
2015-03-16 07:25:29 +0300

    Repo structure changed, makefiles added

---
+++ Makefile
@@ -0,0 +1,4 @@
+all: cat/cat revwords/revwords filter/filter
+
+cat/cat revwords/revwords filter/filter:
+	$(MAKE) -C $(dir $@) $(notdir $@)
---
+++ revwords/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "helpers.h"
+#include "helpers.c"
+
+
+char word[1024];
+char separator = ' ';
+int pos = 0;
+
+int main()
+{
+    char buffer[1024];
+    int counter = 1;
+    while (counter > 0) {
+        counter = read_to_char(STDIN_FILENO, buffer, sizeof(buffer), separator);
+
+        if (counter < 0) {
+            printf("read error");
+            exit(1);
+        }
+        int i;
+        for (i = 0; i < counter; i++) {
+            if (buffer[i] == separator) {
+                if (pos != 0) write_reversed();
+                pos = 0;
+                write_(STDOUT_FILENO, separator, 1);
+            } else {
+                word[pos] = buffer[i];
+                pos++;
+            }
+
+        }
+    }
+
+    if (pos != 0) {
+        write_reversed();
+    }
+
+    return 0;
+}
+
+int write_reversed() {
+    int i;
+    for (i = 0; i < pos / 2; i++) {
+        char temp = word[i];
+        word[i] = word[pos-i-1];
+        word[pos-i-1] = temp;
+    }
+    write_(STDOUT_FILENO, word, pos);
+}
+

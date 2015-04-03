4cd765f93aa0530386696f2b5e088c8ad98b3099
Alex Sholokhov <ak.sholokhov@gmail.com>
2015-04-03 04:18:47 +0400

    Another minor bug fixes

--- bufcat/bufcat.c
+++ bufcat/bufcat.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <bufio.h>
 int main () {
-    struct buf_t * buf = buf_new(4096);
-    int retcode = 0;
+    struct buf_t * buf = buf_new(2048);
+    int ret = 0;
     int read_counter;
     int write_conter;
     do {
         read_counter = buf_fill(STDIN_FILENO, buf, buf_capacity(buf));
         write_conter = buf_flush(STDOUT_FILENO, buf, buf_size(buf));
-        if (read_counter == -1 || read_counter == -1) {
-           retcode = 1;
+        if (read_counter == -1 || write_counter == -1) {
+           ret = 1;
            break;
         }
     } while (read_counter > 0);
     buf_free(buf);
-    return retcode;
+    return ret;
 }

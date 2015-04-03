630df4ade764be98ffeb93d96a050a0452d1b7b2
Alex Sholokhov <ak.sholokhov@gmail.com>
2015-04-03 04:22:33 +0400

    Conter fixed

--- bufcat/bufcat.c
+++ bufcat/bufcat.c
@@ -5,10 +5,10 @@ int main () {
     struct buf_t * buf = buf_new(2048);
     int ret = 0;
     int read_counter;
-    int write_conter;
+    int write_counter;
     do {
         read_counter = buf_fill(STDIN_FILENO, buf, buf_capacity(buf));
-        write_conter = buf_flush(STDOUT_FILENO, buf, buf_size(buf));
+        write_counter = buf_flush(STDOUT_FILENO, buf, buf_size(buf));
         if (read_counter == -1 || write_counter == -1) {
            ret = 1;
            break;

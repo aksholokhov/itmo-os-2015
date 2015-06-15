b147c26fea3d095dc21b4065f32bf63b2eca4d6a
Alex Sholokhov <ak.sholokhov@gmail.com>
2015-06-15 20:41:38 +0300

    new training: cat, which replaces all aa by b in infinite stream

---
+++ cat/revchar.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUF_SIZE 4
+
+int main () {
+    char buf[BUF_SIZE];
+    int shift = BUF_SIZE/2;
+    int sp = 0;
+
+    int read_counter;
+    int write_counter;
+    int maybe_found = 0;
+
+    do {
+        read_counter = read(STDIN_FILENO, buf, sizeof(buf)/2);
+        if (read_counter == -1) {
+            exit(1);
+        }
+        
+        sp = 0;
+
+        for (int i = 0; i < read_counter; i++) {
+            if (buf[i] == 'a') {
+                if (maybe_found == 0) {
+                    maybe_found = 1;
+                } else {
+                    buf[shift + sp] = 'b';
+                    sp++;
+                    maybe_found = 0;
+                }
+            } else {
+                if (maybe_found == 1) {
+                    buf[shift + sp] = 'a';
+                    maybe_found = 0;
+                    sp++;
+                }
+                buf[shift + sp] = buf[i];
+                sp++;
+            }
+        }
+                
+        write_counter = write(STDOUT_FILENO, buf + shift, sp);
+    } while (write_counter == sp);   
+
+    return 0;
+}
